2b9f9a8a94ad0e299858f19af1ac1c71e361d545
Marios Kontis <kontism@gmail.com>
2022-05-18 14:13:47 +0100

    Remove uneccessary console.log

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -4,7 +4,6 @@ exports.getArticleByID = (req, res, next) => {
     const { article_id } = req.params;
     fetchArticleById(article_id)
         .then((article) => {
-            console.log(article)
             res.status(200).send({ article });
         })
         .catch(next);
