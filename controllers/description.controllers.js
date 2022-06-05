945ba5e9eca6b8a0419c3b62fd948015e8db46fa
Marios Kontis <kontism@gmail.com>
2022-06-05 09:45:20 +0100

    Minor fix to endpoints description

--- controllers/description.controllers.js
+++ controllers/description.controllers.js
@@ -1,9 +1,5 @@
-const fs = require('fs/promises')
+const desc = require('../endpoints.json')
 
 exports.getDesc = (req, res, next) => {
-    return fs.readFile('/home/marko/Documents/northcoders/backend/be-nc-news/endpoints.json')
-        .then((endPoints) => {
-            const endpoints = JSON.parse(endPoints);
-            res.status(200).send(endpoints)
-        })
-}
+            res.status(200).send(desc)
+        }
